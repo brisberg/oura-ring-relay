7cedf74e95a7354ef894731ab9b405fb612cbc55
brisberg <brandon.risberg@gmail.com>
2024-12-14 00:00:00 -0800

    Adds a simple pytest unit test for the example function

---
+++ functions/test_main.py
@@ -0,0 +1,9 @@
+import os
+from main import on_request_example
+
+def test_on_request_example():
+    os.environ["OURA_API_KEY"] = 'oura123456'
+    resp = on_request_example(None)
+
+    assert(resp.status == '200 OK')
+    assert(resp.get_data(True) == 'OURA_VARIABLE=oura123456')
