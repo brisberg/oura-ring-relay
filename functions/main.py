006439662957fe1c7f74b32a02dcb8ca39d89976
brisberg <brandon.risberg@gmail.com>
2024-12-13 22:58:38 -0800

    Adds a environment variable to the example function

--- functions/main.py
+++ functions/main.py
@@ -3,11 +3,14 @@
 # Deploy with `firebase deploy`
 
 from firebase_functions import https_fn
+from firebase_functions.params import StringParam
 from firebase_admin import initialize_app
 
 initialize_app()
 
+OURA_API_KEY = StringParam("OURA_API_KEY")
+
 
 @https_fn.on_request()
 def on_request_example(req: https_fn.Request) -> https_fn.Response:
-    return https_fn.Response("Hello world!")
+    return https_fn.Response(f'OURA_VARIABLE={OURA_API_KEY.value}')
