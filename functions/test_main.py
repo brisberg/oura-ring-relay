67ab9ad05ffbe107ce950b74f418d12ac55cf54d
brisberg <brandon.risberg@gmail.com>
2024-12-16 14:32:42 -0800

    Adds assertion for the numerical status code

--- functions/test_main.py
+++ functions/test_main.py
@@ -5,5 +5,6 @@ def test_on_request_example():
     os.environ["OURA_API_KEY"] = 'oura123456'
     resp = on_request_example(None)
 
+    assert(resp.status_code == 200)
     assert(resp.status == '200 OK')
     assert(resp.get_data(True) == 'OURA_VARIABLE=oura123456')
